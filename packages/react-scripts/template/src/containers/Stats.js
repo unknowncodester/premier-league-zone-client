cda437e1a56bad91f17dac119462f2eb39489e35
Dan <sgdhardy1991@gmail.com>
2017-07-29 07:25:32 +0100

    league table WIP

--- packages/react-scripts/template/src/containers/Stats.js
+++ packages/react-scripts/template/src/containers/Stats.js
@@ -5,6 +5,15 @@ import Footer from '../components/Footer.js';
 import { Row } from 'react-bootstrap';
 
 class Stats extends Component {
+
+    _renderLeagueTable() {
+        let renderedLeagueTable = [];
+        
+        leagueTable.forEach(function (team) {
+            
+        })
+    }
+
     render() {
         return (
             <Row>
@@ -15,5 +24,40 @@ class Stats extends Component {
         );
     }
 }
+
+const leagueTable = [
+    {
+        "name": "Arsenal",
+        "games": "3",
+        "wins": "1",
+        "draws": "1",
+        "losses": "1",
+        "points": "4"
+    },
+    {
+        "name": "Bournemouth",
+        "games": "1",
+        "wins": "1",
+        "draws": "0",
+        "losses": "0",
+        "points": "3"
+    },
+    {
+        "name": "Burnley",
+        "games": "1",
+        "wins": "0",
+        "draws": "1",
+        "losses": "0",
+        "points": "1"
+    },
+    {
+        "name": "Brighton & Hove Albion",
+        "games": "1",
+        "wins": "0",
+        "draws": "0",
+        "losses": "1",
+        "points": "0"
+    }
+];
 
 export default Stats;
