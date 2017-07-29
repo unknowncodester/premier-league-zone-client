b28cbaa2f0c0cecee13ca9e40c86eddeded5b80d
Dan <sgdhardy1991@gmail.com>
2017-07-29 06:50:36 +0100

    Team page now displaying teams from mocked data

--- packages/react-scripts/template/src/containers/Teams.js
+++ packages/react-scripts/template/src/containers/Teams.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
 import Header from '../components/Header.js';
-import Body from '../components/Body.js';
+import Team from '../components/Team.js';
 import Footer from '../components/Footer.js';
-import { Row } from 'react-bootstrap';
+import { Row, ListGroup } from 'react-bootstrap';
 
 class Teams extends Component {
+
+    _renderTeams() {
+
+        let renderedTeams = [];
+        teams.forEach(function(team) {
+            renderedTeams.push(<Team data={team} />);
+        });
+
+        return renderedTeams;
+    }
+
     render() {
         return (
             <Row>
                 <Header/>
-                Teams coming soon
+                <ListGroup>
+                    {this._renderTeams()}
+                </ListGroup>
                 <Footer/>
             </Row>
         );
     }
 }
+
+const teams =
+[
+    {
+        "id": 1,
+        "name": "Arsenal"
+    },
+    {
+        "id": 2,
+        "name": "Bournemouth",
+    },
+    {
+        "id": 3,
+        "name": "Brighton",
+    },
+];
 
 export default Teams;
