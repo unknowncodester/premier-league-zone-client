27011cd7018dfe5e546d59cc907db2629c2d9f05
Dan <sgdhardy1991@gmail.com>
2017-09-18 22:33:07 +0100

    Added styling to match component

--- src/components/Match.js
+++ src/components/Match.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import {Card, CardActions, CardHeader, CardText, CardMedia, CardTitle} from 'material-ui/Card';
-import {FlatButton} from 'material-ui';
+
+const styles = {
+    chip: {
+        margin: 4,
+    },
+    wrapper: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+};
 
 class Match extends Component {
     render() {
         return (
             <Card>
-                <CardTitle title={this.props.data.home_team.name + ' vs ' + this.props.data.away_team.name} subtitle={this.props.data.date} />
+                <CardTitle
+                    title={this.props.data.home_team.name + ' vs ' + this.props.data.away_team.name}
+                    subtitle={this.props.data.date}
+                />
                 <CardText>
-                    {this.props.data.home_team.name}
-                    {this.props.data.home_team_goals}
-                    {this.props.data.away_team.name}
-                    {this.props.data.away_team_goals}
-                    {this.props.data.date}
+                    {this.props.data.home_team.name + ' ' + this.props.data.home_team_goals}
+                    <br/>
+                    {this.props.data.away_team.name + ' ' + this.props.data.away_team_goals}
                 </CardText>
             </Card>
         );
