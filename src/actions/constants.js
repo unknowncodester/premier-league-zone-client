8053b11c873764bab0fbaf8e9efabe24a5138046
Dan <sgdhardy1991@gmail.com>
2017-09-27 19:27:36 +0100

    adding constants for action names

---
+++ src/actions/constants.js
@@ -0,0 +1,10 @@
+
+const constants = {
+    GET_LEAGUE: 'GET_LEAGUE',
+    GET_TEAMS: 'GET_TEAMS',
+    GET_TOPICS: 'GET_TOPICS',
+    GET_POSTS: 'GET_POSTS',
+    GET_MATCHES: 'GET_MATCHES'
+}
+
+export default constants;
