58ef63f721745b19bd23046b46b12b35ded90bd3
Dan <sgdhardy1991@gmail.com>
2017-10-09 21:52:38 +0100

    Hooking up to redux devtools

--- src/index.js
+++ src/index.js
@@ -5,12 +5,13 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose} from 'redux';
 import appReducers from './reducers/reducers';
 import promise from 'redux-promise';
 import {Provider} from "react-redux";
 
-const store = createStore(appReducers, applyMiddleware(promise));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(appReducers, composeEnhancers(applyMiddleware(promise)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
